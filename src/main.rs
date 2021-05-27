5ec2cd7fb011764b3def00c4212e06bc83369ab2
Tyler Ilunga <tylerilunga1@gmail.com>
2021-05-26 17:11:35 -0700

    threading spawning but fetch_link_from issues within threads

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,63 @@
 use async_recursion::async_recursion;
+use core::panic;
 use hyper::{Body, Client, Error, Uri};
 use hyper_tls::HttpsConnector;
 use lazy_static::lazy_static;
+use num_cpus;
 use regex::Regex;
 use std::collections::HashSet;
-use std::env;
 use std::fs;
-use std::os;
-use std::str;
-use std::thread;
 use std::io::prelude::*;
-use num_cpus;
+use std::process;
+use std::str;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::{JoinHandle, ThreadId};
+
+// TODO: clean this shit up
 
 lazy_static! {
     static ref HREF_PATTERN: Regex = Regex::new(r#"href=['"]([^'"]+?)['"]"#).unwrap();
-    static ref LINK_PATTERN: Regex = Regex::new(r#"http[s]??://.+?[^js|css|jpg|jpeg|png|mp4|mp3]+?"#).unwrap();
+    static ref LINK_PATTERN: Regex =
+        Regex::new(r#"http[s]??://.+?[^js|css|jpg|jpeg|svg|png|mp4|mp3]+?"#).unwrap();
+}
+#[derive(Debug)]
+struct CrawlUpdate {
+    ok_to_crawl: bool,
+    link: String,
+    crawl_number: i32,
+    worker_id: ThreadId,
 }
 
 fn print_fetch_error(site: String, error: Error) {
     println!("****ERROR fetching from site ({}): {:?}", site, error);
 }
 
+fn write_to_output_file(output_file_path: &str, link: String) {
+    let output_file_opts = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&output_file_path);
+    match output_file_opts {
+        Ok(mut file) => {
+            // let link_str: &str = link;
+            if let Err(e) = writeln!(file, "{}", link) {
+                panic!("Failed to write to output file for link {}: {}", link, e);
+            }
+        }
+        Err(e) => {
+            panic!(
+                "Failed to open output file to persist initial links crawled: {}",
+                e
+            );
+        }
+    }
+}
+
 #[async_recursion(?Send)]
-async fn fetch_links_from<Cb: FnOnce(Option<Vec<String>>)>(site: String, callback: Cb)  {
+async fn fetch_links_from<Cb: FnOnce(Option<Vec<String>>)>(site: String, callback: Cb) {
     let mut links: Vec<String> = Vec::new();
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, Body>(https);
@@ -41,15 +76,15 @@ async fn fetch_links_from<Cb: FnOnce(Option<Vec<String>>)>(site: String, callbac
                                 }
                             }
                             callback(Some(links))
-                        }, 
+                        }
                         Err(e) => {
                             println!("Invalid UTF-8 sequence: {}", e)
-                        },
+                        }
                     };
-                },
+                }
                 Err(e) => {
-                      print_fetch_error(site, e);
-                },
+                    print_fetch_error(site, e);
+                }
             };
         }
         Err(e) => {
@@ -67,7 +102,7 @@ async fn fetch_links_from<Cb: FnOnce(Option<Vec<String>>)>(site: String, callbac
     }
 }
 
-#[tokio::main] // As a macro attribute, it's signature is (_attr: TokenStream, item: TokenStream) -> TokenStream so here the item = our function. It can process the token stream of the token including the "async keyword"
+#[tokio::main]
 async fn main() {
     let mut output_file_path = String::from("./sites.txt");
     match std::env::var_os("OUTPUT_FILE_PATH") {
@@ -91,48 +126,126 @@ async fn main() {
     let mut visited_link_set: HashSet<String> = HashSet::new();
     let mut crawl_number: i32 = 0;
 
-    fetch_links_from(seed_link, |result: Option<Vec<String>>| {
-        match result {
-            Some(links) => {
-                let mut total_log_cpus = num_cpus::get();
-                let links_len = links.len();
-                if links_len < total_log_cpus {
-                    total_log_cpus = links_len;
-                }
+    fetch_links_from(seed_link, |result: Option<Vec<String>>| match result {
+        Some(links) => {
+            let mut total_log_cpus = num_cpus::get();
+            let links_len = links.len();
+            if links_len < total_log_cpus {
+                total_log_cpus = links_len;
+            }
 
-                println!("total_log_cpus: {}", total_log_cpus);
+            println!("total_log_cpus: {}", total_log_cpus);
 
-                let first_links = &links[0..total_log_cpus];
-                if total_log_cpus < links_len {
-                    for link in &links[total_log_cpus+1..] {
-                        println!("link: {}", link);
-                        if !visited_link_set.contains(link) {
-                            crawl_number += 1;
-                            visited_link_set.insert(link.clone());
-                            let output_file_opts = fs::OpenOptions::new()
-                                .write(true)
-                                .append(true)
-                                .open(&output_file_path);
-                            match output_file_opts {
-                                Ok(mut file) => {
-                                    // let link_str: &str = link;
-                                    if let Err(e) = writeln!(file, "{}", link) {
-                                        panic!(format!("Failed to write to output file for link: {}", link));
-                                    }
-                    
-                                }, 
-                                Err(e) => {
-                                    panic!("Failed to open output file to persist initial links crawled.");
-                                }
-                            }
-                        }
-
+            let first_links = &links[0..total_log_cpus];
+            if total_log_cpus < links_len {
+                for link in &links[total_log_cpus + 1..] {
+                    if !visited_link_set.contains(link) {
+                        crawl_number += 1;
+                        visited_link_set.insert(link.clone());
+                        write_to_output_file(&output_file_path, link.clone());
                     }
                 }
-            },
-            None => {
-                panic!("Failed to fetch initial links for crawling. Please try another link.")
+            }
+
+            let total_threads = total_log_cpus;
+            let (sender, receiver): (Sender<CrawlUpdate>, Receiver<CrawlUpdate>) = mpsc::channel();
+            let arc_receiver = Arc::new(Mutex::new(receiver));
+            let mut worker_threads: Vec<JoinHandle<()>> = Vec::new();
+
+            for i in 0..total_threads {
+                let link = &first_links[i];
+
+                crawl_number += 1;
+
+                visited_link_set.insert(link.clone());
+
+                let sender_thread_clone = sender.clone();
+                let received_thread_clone = Arc::clone(&arc_receiver);
+                let link_thread_clone = link.clone();
+                let worker_thread = thread::spawn(move || {
+                    let worker_id = thread::current().id();
+
+                    println!(
+                        "worker {:?} fetching links to crawl from: {:?}",
+                        worker_id, link_thread_clone
+                    );
+
+                    // NOTE: left off here (issues from ownership and lifetime of the function below)
+                    fetch_links_from(
+                        link_thread_clone.clone(),
+                        |t_result: Option<Vec<String>>| match t_result {
+                            Some(t_res_links) => {
+                                println!("t_res_links: {:?}", t_res_links);
+                                if t_res_links.len() == 0 {
+                                    return;
+                                }
+                                for t_link in t_res_links {
+                                    let send_result = sender_thread_clone.send(CrawlUpdate {
+                                        ok_to_crawl: false,
+                                        crawl_number: crawl_number,
+                                        link: t_link.clone(),
+                                        worker_id: worker_id.clone(),
+                                    });
+                                    match send_result {
+                                        Ok(_) => {
+                                            println!(
+                                                "worker {:?} sent CrawlUpdate through channel",
+                                                worker_id.clone()
+                                            );
+                                        }
+                                        Err(e) => {
+                                            panic!(
+                                                "Send error from worker {:?}: {:?}",
+                                                worker_id.clone(),
+                                                e
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                            None => {
+                                println!(
+                                    "worker {:?} received no links to crawl",
+                                    worker_id.clone()
+                                );
+                                process::exit(0);
+                            }
+                        },
+                    );
+
+                    let wt_recv = received_thread_clone.lock().unwrap();
+                    for message in wt_recv.recv() {
+                        println!(
+                            "worker {:?} received a message: {:?}",
+                            worker_id.clone(),
+                            message
+                        );
+                    }
+                });
+
+                worker_threads.push(worker_thread);
+            }
+
+            let recv = arc_receiver.lock().unwrap();
+            for message in recv.recv() {
+                println!("(PARENT) Received a crawl update message: {:?}", message);
+            }
+
+            for thread in worker_threads {
+                let worker_id = thread.thread().id();
+                match thread.join() {
+                    Ok(_) => {
+                        println!("worker {:?} exited successfully", worker_id.clone(),);
+                    }
+                    Err(e) => {
+                        println!("worker {:?} exited with error: {:?}", worker_id.clone(), e);
+                    }
+                }
             }
         }
-    }).await;
+        None => {
+            panic!("Failed to fetch initial links for crawling. Please try another link.")
+        }
+    })
+    .await;
 }
